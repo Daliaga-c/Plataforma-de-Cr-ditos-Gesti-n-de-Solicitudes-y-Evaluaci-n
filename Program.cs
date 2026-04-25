98f1d6525efdb67ac7912da81fca6a55a7e1eb92
Daliaga-c <diego_aliaga@usmp.pe>
2026-04-25 12:49:23 -0500

    Ejercicio 2

--- Program.cs
+++ Program.cs
@@ -35,7 +35,7 @@ using (var scope = app.Services.CreateScope())
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseMigrationsEndPoint();
+    // app.UseMigrationsEndPoint(); // Desactivado para que no bloquee la pantalla
 }
 else
 {
