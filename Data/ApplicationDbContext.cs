7566084353b9285f5027826b448b8446cbb684d8
Daliaga-c <diego_aliaga@usmp.pe>
2026-04-25 11:41:58 -0500

    Base del proyecto

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Plataforma_de_Créditos___Gestión_de_Solicitudes_y_Evaluación.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+{
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Plataforma_de_Créditos___Gestión_de_Solicitudes_y_Evaluación.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
